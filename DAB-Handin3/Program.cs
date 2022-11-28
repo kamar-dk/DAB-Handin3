a6642cd469448229cf144ef60938d931ca381ab1
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-28 13:16:43 +0100

    Update Program.cs

--- DAB-Handin3/Program.cs
+++ DAB-Handin3/Program.cs
@@ -1,6 +1,25 @@
 ﻿using DAB_Handin3.Models;
 using MongoDB.Driver;
 
+string connectionString = "mongodb://localhost:27017";
+string databaseName = "Handin3";
+string collectionName = "Facility";
+
+var client = new MongoClient(connectionString);
+var db = client.GetDatabase(databaseName);
+var collection = db.GetCollection<Facility>(collectionName);
+
+var facility = new Facility { Name = "Uniparken", Latitude = 0000, Longitude = 0000, Decription = "Uni park", Type = "Park" };
+
+await collection.InsertOneAsync(facility);
+
+var results = await collection.FindAsync(_ => true);
+
+foreach (var result in results.ToList())
+{
+    Console.WriteLine($"{result.FacilityId}");
+}
+/*
 namespace DAB_Handin3
 {
     class Program
@@ -13,24 +32,8 @@ namespace DAB_Handin3
 
         static async void test()
         {
-            string connectionString = "mongodb://localhost:27017";
-            string databaseName = "Handin3";
-            string collectionName = "Facility";
-
-            var client = new MongoClient(connectionString);
-            var db = client.GetDatabase(databaseName);
-            var collection = db.GetCollection<Facility>(collectionName);
-
-            var facility = new Facility { Name = "Uniparken", Latitude = 0000, Longitude = 0000, Decription = "Uni park", Type = "Park" };
-
-            await collection.InsertOneAsync(facility);
-
-            var results = await collection.FindAsync(_ => true);
-
-            foreach (var result in results.ToList())
-            {
-                Console.WriteLine($"{result.FacilityId}");
-            }
+            
         }
     }
 }
+*/
