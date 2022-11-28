28d821bb5aa50bfc388c75a08bee4f1e3591c80a
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-28 11:49:34 +0100

    Started på Facility Model

---
+++ DAB-Handin3/Models/Facility.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DAB_Handin3.Models
+{
+    public class Facility
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+        public int FacilityId { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+    }
+}
--- DAB-Handin3/Program.cs
+++ DAB-Handin3/Program.cs
@@ -1,3 +1,14 @@
 ﻿using System;
 using DAB_Handin3.Services;
 using DAB_Handin3.Models;
+
+namespace DAB_Handin3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
