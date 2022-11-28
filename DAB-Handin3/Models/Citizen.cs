1bac1531ec6c29a8fa7edcbee7aa528973d1b76d
Heja Hassan <83606847+MaziatHassan@users.noreply.github.com>
2022-11-28 12:06:48 +0100

    CitizenModel

---
+++ DAB-Handin3/Models/Citizen.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace DAB_Handin3.Models
+{
+    public class Citizen
+    {
+        [BsonId]
+        [BsonRepresentation(BsonType.ObjectId)]
+
+        public string CitizenId { get; set; }
+
+        [BsonElement("Name")]
+        public string Name { get; set; }
+
+        [BsonElement("Email")]
+        public string Email { get; set; }
+
+        [BsonElement("CVR")]
+        public string CVR { get; set; }
+
+        [BsonElement("Category")]
+        public string Category { get; set; }
+
+
+        [BsonElement("PhoneNumber")]
+        public string PhoneNumber { get; set; }
+
+        [BsonElement("Activities")]
+        public IList<string> Activities { get; set; }
+
+
+
+
+    }
+}
