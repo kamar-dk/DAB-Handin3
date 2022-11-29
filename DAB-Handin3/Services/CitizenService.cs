62178f066262434dd32102a4229592521885c38d
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-29 11:59:51 +0100

    Update CitizenService.cs

--- DAB-Handin3/Services/CitizenService.cs
+++ DAB-Handin3/Services/CitizenService.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DAB_Handin3.Models;
+using MongoDB.Driver;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,10 +11,13 @@ namespace DAB_Handin3.Services
     public class CitizenService
     {
         DataAccess db;
+        private const string CitizenCollection = "Citizen";
+        private IMongoCollection<Citizen> Collection;
 
         public CitizenService(DataAccess db)
         {
             this.db = db;
+            Collection = db.ConnectToMongo<Citizen>(CitizenCollection);
         }
 
     }
