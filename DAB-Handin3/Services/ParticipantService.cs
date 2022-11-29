6ce88b91ac6cd903b71e4039681ce45940a9f5ee
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-29 12:14:35 +0100

    Create ParticipantService.cs

---
+++ DAB-Handin3/Services/ParticipantService.cs
@@ -0,0 +1,24 @@
+﻿using DAB_Handin3.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_Handin3.Services
+{
+    public class ParticipantService
+    {
+        DataAccess db;
+
+        private const string ParticipantCollection = "Participant";
+        private IMongoCollection<Participant> Collection;
+
+        public ParticipantService(DataAccess db)
+        {
+            this.db = db;
+            Collection = db.ConnectToMongo<Participant>(ParticipantCollection);
+        }
+    }
+}
