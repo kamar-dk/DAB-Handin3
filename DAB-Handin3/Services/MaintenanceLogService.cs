40ca50efcc158ecc48b0b7bdb914987a5d09a570
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-29 12:13:17 +0100

    Create MaintenanceLogService.cs

---
+++ DAB-Handin3/Services/MaintenanceLogService.cs
@@ -0,0 +1,24 @@
+﻿using DAB_Handin3.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_Handin3.Services
+{
+    public class MaintenanceLogService
+    {
+        DataAccess db;
+
+        private const string MaintenanceLogCollection = "Acticity";
+        private IMongoCollection<MaintenanceLog> Collection;
+
+        public MaintenanceLogService(DataAccess db)
+        {
+            this.db = db;
+            Collection = db.ConnectToMongo<MaintenanceLog>(MaintenanceLogCollection);
+        }
+    }
+}
