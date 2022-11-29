89d928e7212ad38955651ff5e3a8f61c5bc7b228
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-29 12:04:51 +0100

    Create ActivityService.cs

---
+++ DAB-Handin3/Services/ActivityService.cs
@@ -0,0 +1,24 @@
+﻿using DAB_Handin3.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_Handin3.Services
+{
+    public class ActivityService
+    {
+        DataAccess db;
+
+        private const string ActivityCollection = "Acticity";
+        private IMongoCollection<Activity> Collection;
+
+        public ActivityService(DataAccess db)
+        {
+            this.db = db;
+            Collection = db.ConnectToMongo<Activity>(ActivityCollection);
+        }
+    }
+}
