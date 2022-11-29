60f21a88548ef37e1e9bc6cf8ac0a67b8f167710
KasperMartensen <tigriss.tiger@gmail.com>
2022-11-29 12:15:41 +0100

    Create PersonnelService.cs

---
+++ DAB-Handin3/Services/PersonnelService.cs
@@ -0,0 +1,24 @@
+﻿using DAB_Handin3.Models;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAB_Handin3.Services
+{
+    public class PersonnelService
+    {
+        DataAccess db;
+
+        private const string PersonnelCollection = "Personel";
+        private IMongoCollection<Personnel> Collection;
+
+        public PersonnelService(DataAccess db)
+        {
+            this.db = db;
+            Collection = db.ConnectToMongo<Personnel>(PersonnelCollection);
+        }
+    }
+}
